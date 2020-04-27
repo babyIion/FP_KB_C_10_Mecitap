1d5a23a587e5b66ad3f36b7b01a4f2be5e3703af
babylion <btarialiya93@gmail.com>
2020-04-27 20:03:31 +0700

    add grid + asset

---
+++ Mecitap/Assets/Scripts/PuzzleBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleBoard : MonoBehaviour
+{
+    public int width, height;
+
+    //kartu-kartu
+    private Card[,] boardCards;
+    //public GameObject[,] BoardPieces { get { return boardPieces;  } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardCards = new Card[width, height];   
+    }
+
+}
