904660a906168455869107c3a0d9eec017a55a09
babylion <btarialiya93@gmail.com>
2020-04-28 08:12:07 +0700

    add enemy follow

--- Madness Dungeon/Assets/Scripts/Player.cs
+++ Madness Dungeon/Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 3;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
