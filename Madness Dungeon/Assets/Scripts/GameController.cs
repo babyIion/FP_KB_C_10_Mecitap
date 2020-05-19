4aae531620d55c6f78e1920de147070825e993f5
babylion <btarialiya93@gmail.com>
2020-05-19 16:10:19 +0700

    add pause and level3

--- Madness Dungeon/Assets/Scripts/GameController.cs
+++ Madness Dungeon/Assets/Scripts/GameController.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public static bool GameIsPause = false;
+    public GameObject PopUpPause;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
     }
 
     public void QuitGame()
@@ -19,14 +23,49 @@ public class GameController : MonoBehaviour
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
     }
 
     public void Replay()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+        Time.timeScale = 1f;
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+        Time.timeScale = 1f;
+    }
+
+    public void Level()
+    {
+        SceneManager.LoadScene("Level");
+        Time.timeScale = 1f;
+    }
+
+    public void Resume()
+    {
+        PopUpPause.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+
+    public void Pause()
+    {
+        PopUpPause.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
     }
 }
